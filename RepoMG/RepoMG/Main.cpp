8a55166b3e94973e1807417118a6afe917d772bd
AlexCioca <alex.cioca18@gmail.com>
2022-01-01 22:15:30 +0200

    resolving bad commit

--- RepoMG/RepoMG/Main.cpp
+++ RepoMG/RepoMG/Main.cpp
@@ -34,6 +34,8 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+
+
 int main()
 {
 	std::cout << "<ENTER> Start the train movement\n<BACKSPACE> Stop the train movement\n<1> Driver Camera\n<2> Outside Camera\n<3> Free Camera\n<+> Increase train speed\n<-> Decrease train speed\n";
@@ -125,6 +127,8 @@ int main()
 		 1.0f, -1.0f,  1.0f
 	};
 
+	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
 	// build and compile shaders
 	// -------------------------
 	Shader skyboxShader("skybox.vs", "skybox.fs");
@@ -135,6 +139,10 @@ int main()
 	Shader bvSignShader("model.vs", "model.fs");
 	Shader bucSignShader("model.vs", "model.fs");
 	Shader tomShader("model.vs", "model.fs");
+	Shader lightingShader("PhongLight.vs", "PhongLight.fs");
+	Shader lightCubeShader("Lamp.vs", "Lamp.fs");
+	Shader shader("ShadowMapping.vs", "ShadowMapping.fs");
+	Shader simpleDepthShader("ShadowMappingDepth.vs", "ShadowMappingDepth.fs");
 
 	// skybox VAO
 	unsigned int skyboxVAO, skyboxVBO;
@@ -158,6 +166,34 @@ int main()
 	Model secondStation(localPath.string() + "/Resources/station/milwaukeeroaddepot.obj");
 	Model bvSign(localPath.string() + "/Resources/station/bvSign/ExitSign_HiPoly.obj");
 	Model bucSign(localPath.string() + "/Resources/station/bucSign/ExitSign_HiPoly.obj");
+	// configure depth map FBO
+	// -----------------------
+	const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+	unsigned int depthMapFBO;
+	glGenFramebuffers(1, &depthMapFBO);
+	// create depth texture
+	unsigned int depthMap;
+	glGenTextures(1, &depthMap);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+	float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+	// attach depth texture as FBO's depth buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	// shader configuration
+		// --------------------
+	shader.Shader::use();
+	shader.Shader::setInt("diffuseTexture", 0);
+	shader.Shader::setInt("shadowMap", 1);
 
 	std::vector<std::string> faces
 	{
@@ -195,6 +231,13 @@ int main()
 
 	unsigned int key = 0;
 
+	unsigned int lightCubeVAO;
+	glGenVertexArrays(1, &lightCubeVAO);
+	glBindVertexArray(lightCubeVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -208,6 +251,34 @@ int main()
 		// input
 		// -----
 		processInput(window);
+		
+		lightingShader.use();
+		lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lightingShader.setVec3("lightPos", lightPos);
+		lightingShader.setVec3("viewPos", camera.Position);
+
+		// view/projection transformations
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 3000.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		lightingShader.setMat4("projection", projection);
+		lightingShader.setMat4("view", view);
+
+		// world transformation
+		glm::mat4 model = glm::mat4(1.0f);
+		lightingShader.setMat4("model", model);
+
+		// also draw the lamp object
+		lightCubeShader.use();
+		lightCubeShader.setMat4("projection", projection);
+		lightCubeShader.setMat4("view", view);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(10.0f)); 
+		lightCubeShader.setMat4("model", model);
+
+		glBindVertexArray(lightCubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		// render
 		// ------
@@ -220,8 +291,6 @@ int main()
 		terrainShader.use();
 		tomShader.use();
 
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 3000.0f);
-		glm::mat4 view = camera.GetViewMatrix();
 		trainShader.setMat4("projection", projection);
 		trainShader.setMat4("view", view);
 		terrainShader.setMat4("projection", projection);
@@ -239,6 +308,17 @@ int main()
 		glm::mat4 _ndStation = glm::mat4(1.0f);
 		glm::mat4 _bvSign = glm::mat4(1.0f);
 		glm::mat4 _bucSign = glm::mat4(1.0f);
+
+
+
+
+
+
+
+
+
+
+
 
 		// train
 		if (!start)
@@ -260,6 +340,7 @@ int main()
 		tom.Draw(tomShader);
 
 		// terrain
+
 		_terrain = glm::translate(_terrain, glm::vec3(650.0f, -38.0f, -750.0f));
 		_terrain = glm::scale(_terrain, glm::vec3(2500.0f, 2500.0f, 2500.0f));
 		terrainShader.setMat4("model", _terrain);
@@ -292,6 +373,48 @@ int main()
 		_bvSign = glm::rotate(_bvSign, glm::radians(10.0f), glm::vec3(0, 1, 0));
 		bvSignShader.setMat4("model", _bvSign);
 		bvSign.Draw(bucSignShader);
+
+		
+		//// 1. render depth of scene to texture (from light's perspective)
+		//// --------------------------------------------------------------
+		//	glm::mat4 lightProjection, lightView;
+		//glm::mat4 lightSpaceMatrix;
+		//float near_plane = 1.0f, far_plane = 7.5f;
+		////lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane); // note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
+		//lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+		//lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
+		//lightSpaceMatrix = lightProjection * lightView;
+		//// render scene from light's point of view
+		//simpleDepthShader.use();
+		//simpleDepthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+
+		//glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		//glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		//glClear(GL_DEPTH_BUFFER_BIT);
+		//glActiveTexture(GL_TEXTURE0);
+		//glBindTexture(GL_TEXTURE_2D, cubemapTexture);
+		//
+
+		//// reset viewport
+		//glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//// 2. render scene as normal using the generated depth/shadow map  
+		//
+		//glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//shader.use();
+		//shader.setMat4("projection", projection);
+		//shader.setMat4("view", view);
+		//// set light uniforms
+		//shader.setVec3("viewPos", camera.Position);
+		//shader.setVec3("lightPos", lightPos);
+		//shader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
+		//glActiveTexture(GL_TEXTURE0);
+		//glBindTexture(GL_TEXTURE_2D, cubemapTexture);
+		//glActiveTexture(GL_TEXTURE1);
+		//glBindTexture(GL_TEXTURE_2D, depthMap);
+		
 
 		if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) // driver camera
 			key = 1;
@@ -327,7 +450,7 @@ int main()
 		skyboxShader.setMat4("projection", projection);
 		// skybox cube
 		glBindVertexArray(skyboxVAO);
-		glActiveTexture(GL_TEXTURE0);
+		//glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
